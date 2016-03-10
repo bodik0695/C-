012ccff58ccad3dc072df96801b4861813652b2b
bodik0695 <bodik9595@gmail.com>
2016-03-11 00:52:01 +0200

    Version 1.0

---
+++ BaseEmployees/Interfeces/XML/IViewRecordStaffMemberXML.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace BaseEmployees
+{
+    public interface IViewRecordSpecificEmployeeXML
+    {
+        void ViewRecSpecEmpXML(string name, string surname, XmlSerializer xs);
+    }
+}
